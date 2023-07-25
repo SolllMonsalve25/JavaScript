7f2f8f7bf473ffa06bda412ad556cc3a19268fc8
SolMonsalve <solanyimonsalve25@gmail.com>
2023-07-24 22:14:18 -0500

    se crea documento e inicio con javascript

---
+++ js/script.js
@@ -0,0 +1,60 @@
+//console.log("Analisis y Desarrollo de Software");
+
+//alert("Analisis y Desarrollo de Software");
+
+//document.write("Analisis y Desarrollo de Software");
+
+//para comentar en una linea de codigo
+
+/* este tipo de comentario se trabaja en diferentes lineas de codigos */
+
+//declarar variables var
+
+
+
+//var edad=33;
+
+//const nom="Sol";
+//let num=2.5;
+//booleano='ADSO';
+
+/*unir dos cadenas de texto
+const nom="Sol"
+const ape='Monsalve'
+alert("mi nombre es "+nom+" y mi apellido es "+ape) */
+
+/*unir dos cadenas de texto 
+const num1=5
+const num2=3
+const divi = num1*num2
+parseInt(divi)
+alert("la division es: "+divi) */
+
+let nom=prompt("Escriba el nombre")
+
+/*mostrar en una alerta nombres y apellidos completos, edad, peso 
+y direccion utilizando la funcion prompt*/
+
+let ape=prompt("Escriba el apellidos")
+let edad=prompt("Escriba la edad")
+let peso=prompt("Escriba el peso")
+let direccion=prompt("Escriba la direccion")
+alert("mi nombre es: "+nom+ "mi apellido es "+ape+ "mi edad es "+edad+ "mi peso es "+peso+ "mi direccion es "+direccion) 
+
+
+
+
+
+
+//console.log(typeof booleno); typeof es para definir tipo de letra
+ 
+
+//alert("mi edad es: " +edad);
+//document.write("mi edad es:" +edad);
+//el simbolo "+" va a funcionar para concatenar
+
+
+
+//const edad2=22;
+
+//let edad=33;
